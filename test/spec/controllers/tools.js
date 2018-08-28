51da0dc02ea526df1f78e46008651e9bd9b21eef
purplenimbus <you@example.com>
2018-08-28 09:06:40 -0700

    stage tests for controllers , directives and services

--- test/spec/controllers/tools.js
+++ test/spec/controllers/tools.js
@@ -1,23 +1,23 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: ToolsCtrl', function () {
 
   // load the controller's module
   beforeEach(module('nimbusEmsApp'));
 
-  var AboutCtrl,
+  var ToolsCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    ToolsCtrl = $controller('ToolsCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(AboutCtrl.awesomeThings.length).toBe(3);
+    expect(ToolsCtrl.awesomeThings.length).toBe(3);
   });
 });
