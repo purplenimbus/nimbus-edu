fdb40bfe1e5b79c0cd3dbdabbf294ff1b43426e6
purplenimbus <you@example.com>
2018-08-29 09:23:07 -0700

    style user-pill directive

--- app/scripts/directives/letterthumb.js
+++ app/scripts/directives/letterthumb.js
@@ -9,7 +9,7 @@
 angular.module('nimbusEduApp')
   .directive('letterThumb', function () {
     return {
-      template: '<div class="letter-thumb uk-inline uk-border-rounded uk-section-primary"><span class="uk-logo uk-position-center uk-text-light">{{user.firstname.charAt(0) | uppercase}}</span></div>',
+      template: '<div title="{{ user.firstname }} {{ user.lastname }}" class="letter-thumb uk-inline uk-border-rounded uk-section-primary"><span class="uk-logo uk-position-center uk-text-light">{{user.firstname.charAt(0) | uppercase}}</span></div>',
       scope:{user:'=user'},
       restrict: 'E',
       link: function postLink(scope, element) {			
