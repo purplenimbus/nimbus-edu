6fed0b1c0a7b38409cd4195b48e3494d3ff6aef9
purplenimbus <you@example.com>
2018-09-16 07:50:58 -0700

    display widgets responsivley on mobile and remove urls for classes

--- app/scripts/directives/stat-card.js
+++ app/scripts/directives/stat-card.js
@@ -34,7 +34,7 @@ angular.module('nimbusEduApp')
   		body += '</div>';
   		body += '</div>';
 
-  		body +=  '<div class="uk-child-width-1-4@m uk-child-width-1-2@m" uk-grid ng-if="data.analysis">';
+  		body +=  '<div class="uk-child-width-1-4@m uk-child-width-1-2@s" uk-grid ng-if="data.analysis">';
       	body += '  <div ng-repeat="metric in data.analysis" class="uk-text-center">';
         body += '	<p class="uk-text-uppercase uk-text-small uk-margin-remove uk-label" style="background-color:{{colors[$index]}}">{{metric.key|uppercase}}</p>';
   		body += '	<p class="uk-logo uk-text-small" style="color:{{colors[$index]}}">{{metric.data.length}}</p>';
